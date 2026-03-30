1fdf23bf87f153a23aafbb84cf024dcdc01ceaa9
Gerard Dunn <gerard.dunn.92@gmail.com>
2026-03-30 14:57:38 +1300

    initial setup and dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider, useAuth } from '@clerk/clerk-react'
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
+import { ConvexReactClient } from 'convex/react'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
   </StrictMode>,
 )
